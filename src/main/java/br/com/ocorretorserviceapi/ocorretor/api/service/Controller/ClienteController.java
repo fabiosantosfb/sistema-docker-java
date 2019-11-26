628d50a65b697ef99ec970484e76612f17b99e49
fabianovirginio <fabiano@voxtecnologia.com.br>
2019-11-26 09:45:39 -0300

    adicionando documentação da api com swagger

--- src/main/java/br/com/ocorretorserviceapi/ocorretor/api/service/Controller/ClienteController.java
+++ src/main/java/br/com/ocorretorserviceapi/ocorretor/api/service/Controller/ClienteController.java
@@ -1,32 +1,48 @@
 package br.com.ocorretorserviceapi.ocorretor.api.service.Controller;
 
+import br.com.ocorretorserviceapi.ocorretor.api.service.Model.Usuario.Cliente;
+import br.com.ocorretorserviceapi.ocorretor.api.service.Model.Usuario.ClienteResponse;
+import br.com.ocorretorserviceapi.ocorretor.api.service.Utils.Api.Service.Superlogica.ClienteSuperlogicaService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.bind.annotation.*;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 
 @CrossOrigin(origins = "*")
 @RestController
-@RequestMapping(value = "/api/client")
+@RequestMapping(value = "/api/ocorretor")
+@Import({ClienteSuperlogicaService.class})
+@Api(value = "API REST - Manipular dados dos clientes do o-corretor")
 public class ClienteController {
-//    @Autowired
-//    ClienteRepositoryInterface clienteRespository;
+    @Autowired
+    ClienteSuperlogicaService clienteSuperlogicaService;
 
-//    @GetMapping("/listar/clientes")
-//    public List<Cliente> listarCliente() {
-//        return clienteRespository.findAll();
-//    }
-//
-//    @GetMapping("/buscar/cliente/{id}")
-//    public Cliente buscarCliente(@PathVariable(value = "id") long id) {
-//        return clienteRespository.findById(id);
-//    }
-//
-//    @PutMapping("/aditar/cliente")
-//    public Cliente atualizarCliente(@RequestBody @Valid Cliente cliente) {
-//        return clienteRespository.save(cliente);
-//    }
-//
-//    @PostMapping("/salvar/cliente")
-//    public Cliente salvarCliente(@RequestBody @Valid Cliente cliente) {
-//        return clienteRespository.save(cliente);
-//    }
+    @ApiOperation(value = "Busca cliente pelo id de identificação")
+    @GetMapping("/buscar/cliente/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public ClienteResponse buscarCliente(@PathVariable(value = "id") long id) {
+        return this.clienteSuperlogicaService.buscarClientePoId(id);
+    }
+
+    @ApiOperation(value = "Registra cliente")
+    @PostMapping("/criar/cliente/")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response criarCliente(@PathVariable(value = "cliente") Cliente cliente) {
+        return Response.status(Response.Status.CREATED).build();
+    }
+
+    @ApiOperation(value = "Atualizar cliente")
+    @PutMapping("/atualizar/cliente/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response update(@PathVariable("id") long id, Cliente cliente) {
+        return Response.status(Response.Status.CREATED).build();
+    }
 }
--- src/main/java/br/com/ocorretorserviceapi/ocorretor/api/service/Controller/SuperlogicaClienteController.java
+++ src/main/java/br/com/ocorretorserviceapi/ocorretor/api/service/Controller/SuperlogicaClienteController.java
@@ -3,9 +3,9 @@ package br.com.ocorretorserviceapi.ocorretor.api.service.Controller;
 import br.com.ocorretorserviceapi.ocorretor.api.service.Model.Usuario.Cliente;
 import br.com.ocorretorserviceapi.ocorretor.api.service.Model.Usuario.ClienteResponse;
 import br.com.ocorretorserviceapi.ocorretor.api.service.Utils.Api.Service.Superlogica.ClienteSuperlogicaService;
-import org.json.JSONArray;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.json.JSONException;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.bind.annotation.*;
@@ -14,36 +14,40 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.ArrayList;
-import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
-@RequestMapping(value = "/api")
+@RequestMapping(value = "/api/superlogica")
 @Import({ClienteSuperlogicaService.class})
+@Api(value = "API REST - Comunicação com superlogica para tratar dados dos clientes")
 public class SuperlogicaClienteController {
     @Autowired
     ClienteSuperlogicaService clienteSuperlogicaService;
 
-    @GetMapping("/atualizar-base-ocorretor/super-logica/clientes")
+    @ApiOperation(value = "Retorna todos os clientes registrado no superlogica para atualizar a base de dados do o-corretor")
+    @GetMapping("/atualizar-base-ocorretor/clientes")
     @Produces(MediaType.APPLICATION_JSON)
     public Response atualizarBaseClienteOcorretor() throws JSONException {
         return Response.status(Response.Status.CREATED).build();
     }
 
-    @GetMapping("/buscar/cliente/super-logica/{id}")
+    @ApiOperation(value = "Busca cliente no superlogica pelo id de identificação")
+    @GetMapping("/buscar/cliente/{id}")
     @Produces(MediaType.APPLICATION_JSON)
     public ClienteResponse buscarCliente(@PathVariable(value = "id") long id) {
         return this.clienteSuperlogicaService.buscarClientePoId(id);
     }
 
-    @PostMapping("/criar/cliente/super-logica/")
+    @ApiOperation(value = "Registra cliente no superlogica")
+    @PostMapping("/criar/cliente/")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public Response criarCliente(@PathVariable(value = "cliente") Cliente cliente) {
         return Response.status(Response.Status.CREATED).build();
     }
 
-    @PutMapping("/user/{id}")
+    @ApiOperation(value = "Atualizar cliente no superlogica")
+    @PutMapping("/atualizar/cliente/{id}")
     @Consumes(MediaType.APPLICATION_JSON)
     public Response update(@PathVariable("id") long id, Cliente cliente) {
         return Response.status(Response.Status.CREATED).build();
